07a2f2f3bef26eb3de10c6166309ff0fd9366a5c
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2024-04-16 18:38:07 +0900

    [level 4] Title: [3차] 자동완성, Time: 242.69 ms, Memory: 310 MB -BaekjoonHub

---
+++ 프로그래머스/4/17685. ［3차］ 자동완성/［3차］ 자동완성.java
@@ -0,0 +1,65 @@
+import java.util.HashMap;
+class Solution {
+    public int solution(String[] words) {
+        Trie trie = new Trie();
+        int answer = 0;
+
+        // Insert words into trie
+        for (String word : words) {
+            trie.insert(word);
+        }
+
+        for (String word : words){
+           answer += trie.findMinInput(word);
+        }
+        return answer;
+    }
+}
+
+class TrieNode {
+    HashMap<Character, TrieNode> children;
+    boolean isEndOfWord;
+    int wordCount; // 각 노드에서 해당 노드를 포함하는 단어의 개수를 추적합니다.
+
+    public TrieNode() {
+        children = new HashMap<>();
+        isEndOfWord = false;
+        wordCount = 0;
+    }
+}
+
+class Trie {
+    TrieNode root;
+
+    public Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (char ch : word.toCharArray()) {
+            if (!node.children.containsKey(ch)) {
+                node.children.put(ch, new TrieNode());
+            }
+            node = node.children.get(ch);
+            node.wordCount++; // 단어가 삽입되었으므로 해당 노드의 단어 개수를 증가시킵니다.
+        }
+        node.isEndOfWord = true;
+    }
+
+    public int findMinInput(String word) {
+        TrieNode node = root;
+        int count = 0;
+        for (char ch : word.toCharArray()) {
+            if (node.children.containsKey(ch)) {
+                node = node.children.get(ch);
+                count++;
+                if (node.wordCount == 1) // Unique prefix found, stop
+                    break;
+            } else {
+                break;
+            }
+        }
+        return count;
+    }
+}
