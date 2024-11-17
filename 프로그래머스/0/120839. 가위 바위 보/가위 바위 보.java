d374c0ab262e81ce780dff97e86dada76fc5d2fa
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2024-11-17 20:55:33 +0900

    [level 0] Title: 가위 바위 보, Time: 0.07 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,26 +1,20 @@
 class Solution {
     public String solution(String rsp) {
-        String answer = "";
-        
-        for(String string : rsp.split("")){
-            
-            switch (string) {
-            case "2":
-                answer += "0";
-                break;
-            case "0":
-                answer += "5";
-                break;
-            case "5":
-                answer += "2";
-                break;
-            default:
-                answer += "Invalid input";
-                break;
+        StringBuilder answer = new StringBuilder();
+
+        for (char ch : rsp.toCharArray()) {
+            switch (ch) {
+                case '2':
+                    answer.append("0");
+                    break;
+                case '0':
+                    answer.append("5");
+                    break;
+                case '5':
+                    answer.append("2");
+                    break;
+            }
         }
-            
-        }
-        
-        return answer;
+        return answer.toString();
     }
 }
