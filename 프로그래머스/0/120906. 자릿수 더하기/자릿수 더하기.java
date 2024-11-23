dd39061a6840549c37dbdb9e5eaf1b009d38a9e3
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2024-11-23 22:54:44 +0900

    [level 0] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120906. 자릿수 더하기/자릿수 더하기.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        for(char c : String.valueOf(n).toCharArray()){
+            answer += c -'0';
+        }
+        
+        return answer;
+    }
+}
