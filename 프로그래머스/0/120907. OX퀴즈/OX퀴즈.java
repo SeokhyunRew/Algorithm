d30b091f347a9f1f2ca5abd1f6eac44a23190136
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2024-11-23 23:13:26 +0900

    [level 0] Title: OX퀴즈, Time: 0.21 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120907. OX퀴즈/OX퀴즈.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String[] solution(String[] quiz) {
+        String[] answer = new String[quiz.length];
+        String[] temp;
+        
+        for(int i=0; i<quiz.length; i++){
+            temp = quiz[i].split(" ");
+            if(temp[1].equals("+")){
+                if(Integer.parseInt(temp[0])+Integer.parseInt(temp[2])==Integer.parseInt(temp[4])){
+                    answer[i] = "O";
+                }else{
+                    answer[i] = "X";
+                }
+            }else{
+                if(Integer.parseInt(temp[0])-Integer.parseInt(temp[2])==Integer.parseInt(temp[4])){
+                    answer[i] = "O";
+                }else{
+                    answer[i] = "X";
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
