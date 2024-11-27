43ee8662e0088aa7e8c6b2e36ce8e2fbf3066ab5
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2024-11-27 21:32:22 +0900

    [level 0] Title: 안전지대, Time: 0.03 ms, Memory: 79.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120866. 안전지대/안전지대.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int solution(int[][] board) {
+        int n = board.length;
+
+        int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 1) { 
+                    for (int k = 0; k < 8; k++) { 
+                        int ni = i + dx[k];
+                        int nj = j + dy[k];
+                        if (ni >= 0 && ni < n && nj >= 0 && nj < n && board[ni][nj] == 0) {
+                            board[ni][nj] = -1; 
+                        }
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 0) { 
+                    answer++;
+                }
+            }
+        }
+
+        return answer;
+    }
+}
