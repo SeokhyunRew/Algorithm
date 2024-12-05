0341e5c600f9d80ed291a188bb308a6b9ecf479d
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2024-12-05 22:01:23 +0900

    [level 0] Title: 배열 만들기 2, Time: 0.48 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -1,41 +1,32 @@
 class Solution {
     public int[] solution(int l, int r) {
-        StringBuilder sb = new StringBuilder();
-        String temp;
+        int start = String.valueOf(l).length();
+        int end = String.valueOf(r).length();
         int count = 0;
-        
-        for(int i=l; i<=r; i++){
-            if(i%5==0){
-                sb.append(i);
-                temp = sb.toString();
-                temp = temp.replace("0", "");
-                temp = temp.replace("5", "");
-                if(temp.equals("")) count++;
-                sb.setLength(0);
-                temp = "";
-            } else continue;
+
+        for (int j = (int) Math.pow(2, start - 1); j < (int) Math.pow(2, end); j++) {
+            int num = Integer.parseInt(Integer.toBinaryString(j)) * 5;
+
+            if (num >= l && num <= r) {
+                count++;
+            }
         }
-        
-        if(count==0) return new int[] {-1};
-        
+
+        if (count == 0) {
+            return new int[]{-1};
+        }
+
         int[] answer = new int[count];
         count = 0;
-        
-        for(int i=l; i<=r; i++){
-            if(i%5==0){
-                sb.append(i);
-                temp = sb.toString();
-                temp = temp.replace("0", "");
-                temp = temp.replace("5", "");
-                if(temp.equals("")){
-                    answer[count] = i;
-                    count ++;
-                } 
-                sb.setLength(0);
-                temp = "";
-            } else continue;
+
+        for (int j = (int) Math.pow(2, start - 1); j < (int) Math.pow(2, end); j++) {
+            int num = Integer.parseInt(Integer.toBinaryString(j)) * 5;
+            if (num >= l && num <= r) {
+                answer[count] = num;
+                count++;
+            }
         }
-        
+
         return answer;
     }
 }
