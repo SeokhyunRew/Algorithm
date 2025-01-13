95b477600349d80bd552a75e5a5cec661c35092a
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-01-13 21:23:57 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,38 +1,14 @@
 class Solution {
     public int solution(int n) {
-        int answer = 0;
         int temp = 0;
-        int count = 0;
         StringBuilder sb = new StringBuilder();
         
-        while(true){
-            if(n-Math.pow(3, temp)<0) break;
-            temp++;
+        while(n>0){
+            temp = n%3;
+            n /= 3;
+            sb.append(temp);
         }
         
-        int[] three = new int[temp];
-        
-        for(int i=0; i<temp; i++){
-            three[i] = (int) Math.pow(3, temp-i-1);
-        }
-        
-        for(int j=0; j<temp; j++){
-            while(true){
-                if(n-three[j]>=0){
-                    n -= three[j];
-                    count++;
-                }else break;
-            }
-            sb.append(count);
-            count = 0;
-        }
-        
-        sb.reverse();
-        
-        for(int h=0; h<temp; h++){
-            answer += three[h]*(sb.charAt(h)-'0');
-        }
-        
-        return answer;
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
