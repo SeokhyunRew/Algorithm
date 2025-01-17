421aaec834be269d60a06f33ea27a86f0404de5d
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-01-17 23:15:33 +0900

    [level 1] Title: 실패율, Time: 6.05 ms, Memory: 107 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,35 +1,36 @@
-import java.util.*;
 class Solution {
     public int[] solution(int N, int[] stages) {
         int[] answer = new int[N];
-        double[][] temp = new double[N][2];
-        int up;
-        int down;
-        double percent = 0;
-        
-        for(int i=1; i<=N; i++){
-            up=0;
-            down=0;
-            for(int j=0; j<stages.length; j++){
-                if(stages[j]>=i) down++;
-                if(stages[j]==i) up++;
-            }
-            percent = (down == 0) ? 0 : (double) up / down;
-            temp[i-1][0] = percent; 
-            temp[i-1][1] = i; 
+        double[] tempArr = new double[N];
+        int arrLength = stages.length;
+        int idx = arrLength;
+        double tempD = 0;
+        int tempI = 0;
+        for (int i = 0; i < arrLength; i++) {
+            int stage = stages[i];
+            if (stage != N + 1)
+                answer[stage - 1] += 1;
         }
-        
-        Arrays.sort(temp, (a, b) -> {
-            if (Double.compare(b[0], a[0]) == 0) {
-                return Double.compare(a[1], b[1]);
-            }
-            return Double.compare(b[0], a[0]);
-        });
-        
-        for(int i=0; i<answer.length; i++){
-            answer[i] = (int) temp[i][1];            
+        for (int i = 0; i < N; i++) {
+            int personNum = answer[i];
+            tempArr[i] = (double) personNum / idx;
+            idx -= personNum;
+            answer[i] = i + 1;
         }
-        
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 1; j < N - i; j++) {
+                if (tempArr[j - 1] < tempArr[j]) {
+                    tempD = tempArr[j - 1];
+                    tempArr[j - 1] = tempArr[j];
+                    tempArr[j] = tempD;
+
+                    tempI = answer[j - 1];
+                    answer[j - 1] = answer[j];
+                    answer[j] = tempI;
+                }
+            }
+        }
         return answer;
     }
 }
