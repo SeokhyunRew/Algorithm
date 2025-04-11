6ff71633fdd45fcb7abca30abe828395b9e667cd
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-04-11 15:38:30 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 공원, Time: 1.40 ms, Memory: 97.5 MB -BaekjoonHub

--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/README.md
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/README.md
@@ -1,10 +1,10 @@
 # [level 1] [PCCE 기출문제] 10번 / 공원 - 340198 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340198#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/340198) 
 
 ### 성능 요약
 
-메모리: 91.3 MB, 시간: 5.17 ms
+메모리: 97.5 MB, 시간: 1.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 04월 11일 15:26:25
+2025년 04월 11일 15:38:28
 
 ### 문제 설명
 
--- 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
+++ 프로그래머스/1/340198. ［PCCE 기출문제］ 10번 ／ 공원/［PCCE 기출문제］ 10번 ／ 공원.java
@@ -1,32 +1,41 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] mats, String[][] park) {
-        int answer = 0;
-        boolean check = true;
-        
-        for(int i=0; i<park.length; i++){
-            for(int j=0; j<park[0].length; j++){
-                if(park[i][j].equals("-1")){
-                    for(int k=0; k<mats.length; k++){
-                        check=true;
-                        
-                        if(mats[k]<=answer) continue;
-                        if(i+mats[k]>park.length||j+mats[k]>park[0].length) continue;
-                        
-                        for(int h=0; h<mats[k]; h++){
-                            for(int l=0; l<mats[k]; l++){
-                                if(!park[i+h][j+l].equals("-1")){
-                                    check=false;
-                                    break;
-                                }      
-                            } 
-                            if(!check) break;
-                        }
-                        if(check) answer=mats[k];
+        int H = park.length;
+        int W = park[0].length;
+        int[][] grid = new int[H][W];
+        int[][] sum = new int[H + 1][W + 1];
+
+        // 1. 문자열을 숫자로 변환
+        for (int i = 0; i < H; i++) {
+            for (int j = 0; j < W; j++) {
+                grid[i][j] = park[i][j].trim().equals("-1") ? 1 : 0;
+            }
+        }
+
+        // 2. 누적합 배열 만들기
+        for (int i = 1; i <= H; i++) {
+            for (int j = 1; j <= W; j++) {
+                sum[i][j] = grid[i-1][j-1] + sum[i-1][j] + sum[i][j-1] - sum[i-1][j-1];
+            }
+        }
+
+        // 3. mats 정렬 (내림차순)
+        Arrays.sort(mats);
+        for (int i = mats.length - 1; i >= 0; i--) {
+            int size = mats[i];
+
+            for (int y = 0; y <= H - size; y++) {
+                for (int x = 0; x <= W - size; x++) {
+                    int areaSum = sum[y + size][x + size] - sum[y][x + size] - sum[y + size][x] + sum[y][x];
+                    if (areaSum == size * size) {
+                        return size; // 가장 큰 정사각형 발견 → 바로 반환
                     }
                 }
             }
         }
-        
-        return (answer==0)?-1:answer;
+
+        return -1;
     }
 }
