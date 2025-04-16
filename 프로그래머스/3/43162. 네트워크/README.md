40d938a02e0c13f77cdc85c6569c04dd74b9256d
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-04-17 01:10:54 +0900

    [level 3] Title: 네트워크, Time: 0.42 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 78 MB, 시간: 0.13 ms
+메모리: 81.9 MB, 시간: 0.42 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 22일 17:09:33
+2025년 04월 17일 01:10:52
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,32 +1,31 @@
-import java.util.*;
-
+import java.util.List;
+import java.util.ArrayList;
 class Solution {
-    static int count = 0;
-    static List<Integer> tempList = new ArrayList<>();
-    static boolean[] visited;
     public int solution(int n, int[][] computers) {
-        visited = new boolean[n];
-
-        for (int i = 0; i < n; i++) {
-            tempList.add(i);
+        int answer = 0;
+        List<List<Integer>> network = new ArrayList<>();
+        boolean[] visited = new boolean[n+1];
+        
+        for(int i=0; i<n; i++) network.add(new ArrayList<>());
+        
+        for(int i=1; i<=n; i++){
+            if(visited[i]) continue;
+            visited = dfs(computers, visited, i, n);
+            answer++;
         }
-
-        for (int j = 0; j < computers.length; j++) {
-            if (!visited[j]) {
-                bfs(computers, j);
-                count++;
-            }
-        }
-        return count;
+        
+        return answer;
     }
     
-    public static void bfs(int[][] computers, int node){
-        visited[node] = true;
-
-        for (int i = 0; i < computers[node].length; i++) {
-            if (computers[node][i] == 1 && !visited[i]) {
-                bfs(computers, i);
-            }
+    private boolean[] dfs(int[][] computers,boolean[] visited, int node, int n){
+        visited[node]=true;
+        
+        for(int i=0; i<n; i++){
+            if(i==node-1) continue;
+            if(computers[node-1][i]==1&&!visited[i+1]) dfs(computers, visited, i+1, n);
         }
+        
+        return visited;
     }
+    
 }
