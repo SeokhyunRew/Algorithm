4c27bda822696fc22aac4b003112a27d4c8c867a
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-04-19 20:32:25 +0900

    [level 2] Title: 더 맵게, Time: 1589.83 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
 
 ### 성능 요약
 
-메모리: 124 MB, 시간: 2737.80 ms
+메모리: 129 MB, 시간: 1589.83 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 16일 16:55:47
+2025년 04월 19일 20:32:22
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,30 +1,17 @@
 import java.util.*;
 class Solution {
     public int solution(int[] scoville, int K) {
-        int mini;
-        int next_mini;
-        int temp;
         int answer = 0;
-
-        // PriorityQueue 생성 (기본값: 최소 힙)
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-
-        // 배열의 모든 요소를 힙에 추가
-        for (int num : scoville) {
-            minHeap.offer(num); // offer() 메서드를 사용하여 요소를 추가
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        
+        for(int num : scoville) queue.add(num);
+        
+        while(queue.size() > 1 && queue.peek()<K){
+            queue.add(queue.poll()+queue.poll()*2);
+            answer++;
         }
         
-        while(minHeap.peek() < K){
-            if(minHeap.size() < 2){
-            return -1;
-        }
-            mini = minHeap.poll();
-            next_mini = minHeap.poll();
-            temp = mini + next_mini*2;
-            minHeap.offer(temp);
-            answer ++;
-        }
-        
-        return answer;
+        return queue.peek() >= K ? answer : -1;
     }
 }
