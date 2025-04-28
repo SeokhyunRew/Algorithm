50bdcc348eefb77420fcd43419d0dc059b21dc5e
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-04-28 17:54:01 +0900

    [level 2] Title: 짝지어 제거하기, Time: 64.63 ms, Memory: 64.7 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -4,16 +4,11 @@ class Solution {
     public int solution(String s) {
         Stack<Character> stack = new Stack<>();
         
-        for (char c : s.toCharArray()) {
-            // 스택이 비어있지 않고, top에 있는 문자와 현재 문자가 같으면
-            if (!stack.isEmpty() && stack.peek() == c) {
-                stack.pop(); // 짝 제거
-            } else {
-                stack.push(c); // 아니면 스택에 추가
-            }
+        for(char c : s.toCharArray()){
+            if(!stack.isEmpty()&&stack.peek()==c) stack.pop();
+            else stack.push(c);
         }
         
-        // 스택이 비어있으면 모두 짝지어 제거된 것
         return stack.isEmpty() ? 1 : 0;
     }
 }
