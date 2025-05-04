55b5d9972b2b5deb0224cb0b63989c1afe18e72b
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-05-04 20:13:00 +0900

    [level 2] Title: 할인 행사, Time: 41.88 ms, Memory: 110 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.java
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.java
@@ -1,23 +1,43 @@
 import java.util.Map;
 import java.util.HashMap;
+
 class Solution {
     public int solution(String[] want, int[] number, String[] discount) {
         int answer = 0;
-        boolean canJoin;
-        Map<String, Integer> wantMap = new HashMap<>();
-        
-        for(int i=0; i<number.length; i++) wantMap.put(want[i], number[i]);
-        
-        for(int i=0; i<=discount.length-10; i++){
-            Map<String, Integer> buyMap = new HashMap<>();
-            canJoin = true;
-            for(int j=0; j<10; j++) buyMap.put(discount[j+i], buyMap.getOrDefault(discount[j+i], 0)+1);
-            for(int k=0; k<want.length; k++){
-                if(buyMap.getOrDefault(want[k],0) < wantMap.get(want[k])) canJoin=false;
-            }
-            if(canJoin) answer++;
+        Map<String, Integer> buyMap = new HashMap<>();
+        int n = discount.length;
+
+        // 초기 10일간의 상품 구성
+        for (int i = 0; i < 10; i++) {
+            buyMap.put(discount[i], buyMap.getOrDefault(discount[i], 0) + 1);
         }
-        
+
+        for (int i = 0; i <= n - 10; i++) {
+            // 현재 윈도우가 조건을 만족하는지 검사
+            boolean canJoin = true;
+            for (int k = 0; k < want.length; k++) {
+                if (buyMap.getOrDefault(want[k], 0) < number[k]) {
+                    canJoin = false;
+                    break;
+                }
+            }
+
+            if (canJoin) answer++;
+
+            // 슬라이딩 윈도우 이동: i일 제거, i+10일 추가
+            if (i + 10 < n) {
+                String out = discount[i];
+                String in = discount[i + 10];
+
+                // 제거
+                buyMap.put(out, buyMap.get(out) - 1);
+                if (buyMap.get(out) == 0) buyMap.remove(out); // 깔끔하게 삭제
+
+                // 추가
+                buyMap.put(in, buyMap.getOrDefault(in, 0) + 1);
+            }
+        }
+
         return answer;
     }
 }
