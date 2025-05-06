32f4bd3c692ee05d2e6dd335934a8a01d89bfb52
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-05-07 00:57:13 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 10.86 ms, Memory: 55.9 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,45 +1,38 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.*;
 class Solution {
     public int solution(int[][] maps) {
+        int[] dx = {-1, 1, 0, 0};
+        int[] dy = {0, 0, -1, 1};
         int n = maps.length;
         int m = maps[0].length;
-        boolean[][] visited = new boolean[n][m];
+        
         Queue<int[]> queue = new LinkedList<>();
         
-        int[] dx = {-1,1,0,0};
-        int[] dy = {0,0,-1,1};
-        
-        queue.offer(new int[]{0, 0, 1});
-        visited[0][0] = true;
+        queue.offer(new int[] {0,0,1});
         
         while(!queue.isEmpty()){
+            
             int[] curr = queue.poll();
-            int x = curr[0];
-            int y = curr[1];
-            int dist = curr[2];
+            int currX = curr[0];
+            int currY = curr[1];
+            int move = curr[2];
             
-            if(x==n-1 && y==m-1) return dist;
+            if(currX==n-1 && currY==m-1) return move;
             
-            for(int i=0; i<dx.length; i++){
-                int nx = x+dx[i];
-                int ny = y+dy[i];
+            for(int i=0; i<4; i++){
+                int newX = currX+dx[i];
+                int newY = currY+dy[i];
                 
-                if(nx>=0&&
-                   ny>=0&&
-                   nx<n&&
-                   ny<m&&
-                   maps[nx][ny]==1&&
-                   !visited[nx][ny]){
-                    
-                    visited[nx][ny]=true;
-                    queue.offer(new int[] {nx,ny,dist+1});
-                    
-                }
+                if(newX>=0 &&
+                   newX<n &&
+                   newY>=0 &&
+                   newY<m &&
+                   maps[newX][newY]!=0){
+                    maps[newX][newY] = 0; // 방문 처리
+                    queue.offer(new int[] {currX+dx[i], currY+dy[i], move+1});
+                } 
             }
         }
-        
         return -1;
     }
 }
