2589c0b4d0031c6cf8d0a475d67d5b5103546da2
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-05-06 23:10:47 +0900

    [level 2] Title: 프로세스, Time: 1.59 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,32 +1,27 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < priorities.length; i++) {
-            queue.add(new int[]{i, priorities[i]}); // [index, priority]
-        }
-
-        Arrays.sort(priorities); // 오름차순 정렬
-        int priorityIndex = priorities.length - 1;
-
-        while (!queue.isEmpty()) {
-            int[] curr = queue.poll();
-            int idx = curr[0];
-            int pri = curr[1];
-
-            if (pri == priorities[priorityIndex]) {
+        int priIndex = priorities.length-1;
+        Queue<Integer> queue = new LinkedList<>();
+        
+        for(int num : priorities) queue.offer(num);
+        
+        Arrays.sort(priorities);
+        
+        while(true){
+            if(queue.peek()==priorities[priIndex]){
+                queue.poll();
+                priIndex--;
                 answer++;
-                priorityIndex--;
-
-                if (idx == location) break;
-            } else {
-                queue.add(curr); // 우선순위 낮으면 뒤로
+                if(location==0) return answer;
+                location--;
+                continue;
             }
+            queue.offer(queue.peek());
+            queue.poll();
+            if(location==0) location=queue.size()-1;
+            else location--;
         }
-
-        return answer;
     }
 }
