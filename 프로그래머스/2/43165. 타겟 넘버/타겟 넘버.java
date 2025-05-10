6c7b6fe56226b915a9ff6e32a07acff7d0ed0638
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-05-11 00:33:56 +0900

    [level 2] Title: 타겟 넘버, Time: 11.45 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,24 +1,23 @@
 class Solution {
-    int answer = 0; // 클래스 전역 변수로 선언
-
+    int answer = 0;
     public int solution(int[] numbers, int target) {
-        dfs(numbers, target, 0, 0);
-        return answer;
+        
+        dfs(0, 0, 1, target, numbers);
+        dfs(0, 0, -1, target, numbers);
+        
+        return answer/2;
     }
-
-    // DFS 함수: 현재 인덱스, 누적합을 받아서 탐색
-    private void dfs(int[] numbers, int target, int depth, int sum) {
-        if (depth == numbers.length) {
-            if (sum == target) {
-                answer++;
-            }
+    
+    private void dfs(int index, int num, int sign, int target, int[] numbers){
+        if(index==numbers.length){
+            if(num==target) answer++;
             return;
-        }
-
-        // 현재 숫자를 더하거나
-        dfs(numbers, target, depth + 1, sum + numbers[depth]);
-
-        // 현재 숫자를 빼거나
-        dfs(numbers, target, depth + 1, sum - numbers[depth]);
+        } 
+        
+        num += numbers[index]*sign;
+        
+        dfs(index+1, num, 1, target, numbers);
+        dfs(index+1, num, -1, target, numbers);
+        
     }
 }
