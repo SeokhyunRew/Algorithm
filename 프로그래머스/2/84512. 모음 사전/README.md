67450cafa405a9d610b5565c7ea264869afdc92a
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-05-11 01:21:24 +0900

    [level 2] Title: 모음 사전, Time: 1.56 ms, Memory: 78.8 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/README.md
+++ 프로그래머스/2/84512. 모음 사전/README.md
@@ -1,10 +1,10 @@
 # [level 2] 모음 사전 - 84512 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/84512) 
 
 ### 성능 요약
 
-메모리: 74.8 MB, 시간: 0.02 ms
+메모리: 78.8 MB, 시간: 1.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 07일 18:30:09
+2025년 05월 11일 01:21:23
 
 ### 문제 설명
 
--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,11 +1,35 @@
+import java.util.*;
+
 class Solution {
-    public int solution(String word) {            
-        String charOrder = "AEIOU";
-        int dictNum = 0;
-        int[] num = {781, 156, 31, 6, 1};
-        for (int i = 0; i < word.length(); i++) {
-            dictNum += charOrder.indexOf(word.charAt(i)) * num[i] + 1;
+    private final char[] VOWELS = {'A','E','I','O','U'};
+    private String target;   
+    private int count = 0;    
+    private int answer = 0;  
+
+    public int solution(String word) {
+        target = word;
+        dfs(new StringBuilder());
+        return answer;
+    }
+
+    private boolean dfs(StringBuilder curr) {
+        if (curr.length() > 0) {
+            count++;
+            if (curr.toString().equals(target)) {
+                answer = count;
+                return true;
+            }
         }
-        return dictNum;
+        if (curr.length() == 5) {
+            return false;
+        }
+        for (char c : VOWELS) {
+            curr.append(c);
+            if (dfs(curr)) {
+                return true;    
+            }
+            curr.deleteCharAt(curr.length() - 1); 
+        }
+        return false;
     }
 }
