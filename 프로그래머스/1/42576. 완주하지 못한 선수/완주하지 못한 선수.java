7f1facf2c7b18ad7695f4f65e9ce91460d028591
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-07-29 17:46:53 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 93.33 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,21 +1,18 @@
 import java.util.*;
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        String answer = "";
-        HashMap<String, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         
-        for(String str : participant){
-            map.put(str, map.getOrDefault(str, 0)+1);
+        for(String s : completion) map.put(s, map.getOrDefault(s, 0)+1);
+        
+        for(String s : participant){
+            if(!map.containsKey(s)) return s;  
+            else{
+                if(map.get(s)==0) return s;
+                map.put(s, map.getOrDefault(s, 0)-1);
+            }
         }
         
-        for(String str : completion){
-            map.put(str, map.get(str)-1);
-        }
-        
-        for(Map.Entry<String, Integer> entry : map.entrySet()){
-            if(entry.getValue()==1) return entry.getKey();
-        }
-         
-        return answer;
+        return "";
     }
 }
