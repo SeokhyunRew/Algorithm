f79ce0c84b9bd1a50117aca5f1a40dc0ec3f09ab
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-08-08 15:57:01 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 9.26 ms, Memory: 56.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -3,36 +3,35 @@ class Solution {
     public int solution(int[][] maps) {
         int[] dx = {-1, 1, 0, 0};
         int[] dy = {0, 0, -1, 1};
-        int n = maps.length;
-        int m = maps[0].length;
-        
+        int xlen = maps.length;
+        int ylen = maps[0].length;
+        boolean[][] visited = new boolean[xlen][ylen];
         Queue<int[]> queue = new LinkedList<>();
         
         queue.offer(new int[] {0,0,1});
+        visited[0][0] = true;
         
         while(!queue.isEmpty()){
-            
             int[] curr = queue.poll();
-            int currX = curr[0];
-            int currY = curr[1];
-            int move = curr[2];
+            int x = curr[0];
+            int y = curr[1];
+            int dist = curr[2];
             
-            if(currX==n-1 && currY==m-1) return move;
+            if(x==xlen-1 && y==ylen-1) return dist;
             
             for(int i=0; i<4; i++){
-                int newX = currX+dx[i];
-                int newY = currY+dy[i];
-                
-                if(newX>=0 &&
-                   newX<n &&
-                   newY>=0 &&
-                   newY<m &&
-                   maps[newX][newY]!=0){
-                    maps[newX][newY] = 0; // 방문 처리
-                    queue.offer(new int[] {currX+dx[i], currY+dy[i], move+1});
-                } 
+                if(x+dx[i] >= 0 &&
+                   x+dx[i] < xlen &&
+                   y+dy[i] >= 0 &&
+                   y+dy[i] < ylen &&
+                   maps[x+dx[i]][y+dy[i]] != 0 &&
+                   !visited[x+dx[i]][y+dy[i]]){
+                    visited[x+dx[i]][y+dy[i]] = true;
+                    queue.offer(new int[] {x+dx[i], y+dy[i], dist+1});
+                }
             }
         }
+        
         return -1;
     }
 }
