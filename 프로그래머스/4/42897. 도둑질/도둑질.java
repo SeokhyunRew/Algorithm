0cd6bf718c843b2bb6656b3c5b6899310307b211
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-09-01 23:47:52 +0900

    [level 4] Title: 도둑질, Time: 23.49 ms, Memory: 94.4 MB -BaekjoonHub

---
+++ 프로그래머스/4/42897. 도둑질/도둑질.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(int[] money) {
+        int[] dp = new int[money.length-1];
+        
+        if(money.length==0) return 0;
+        else if(money.length==1) return money[0];
+        else if(money.length==2) return (money[0]>money[1])?money[0]:money[1];
+        
+        //1번째를 안 골랐을때
+        dp[0] = money[1];
+        dp[1] = Math.max(money[1], money[2]);
+        
+        for(int i=2; i<money.length-1; i++) dp[i] = Math.max(money[i+1]+dp[i-2], dp[i-1]);
+        int firstAns = dp[money.length-2];
+        
+        //마지막을 안고를 때
+        dp[0] = money[0];
+        dp[1] = Math.max(money[0], money[1]);
+        
+        for(int i=2; i<money.length-1; i++) dp[i] = Math.max(money[i]+dp[i-2], dp[i-1]);
+        int secondAns = dp[money.length-2];
+        
+        return (firstAns>secondAns)?firstAns:secondAns;
+    }
+}
