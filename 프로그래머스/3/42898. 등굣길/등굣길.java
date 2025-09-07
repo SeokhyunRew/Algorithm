7b38682b6a77aed98575f1e4dedf4924a9cb65c6
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-09-07 18:04:28 +0900

    [level 3] Title: 등굣길, Time: 0.87 ms, Memory: 54.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        
+        int[][] dp = new int[m+1][n+1];
+        int MOD = 1_000_000_007;
+        
+        boolean[][] puddleMap = new boolean[m+1][n+1];
+        
+        for(int[] arr : puddles) puddleMap[arr[0]][arr[1]] = true;
+        
+        dp[1][1] = 1;
+        
+        for(int i=1; i<m+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(i==1&&j==1) continue;
+                
+                if(puddleMap[i-1][j] && puddleMap[i][j-1]) dp[i][j] = 0;
+                else if(puddleMap[i-1][j]) dp[i][j] = dp[i][j-1]% MOD;
+                else if(puddleMap[i][j-1]) dp[i][j] = dp[i-1][j]% MOD;
+                else dp[i][j] = (dp[i][j-1]+dp[i-1][j])% MOD;
+            }
+        }
+        
+        return dp[m][n];
+    }
+}
