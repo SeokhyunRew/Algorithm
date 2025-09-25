50b8e98af1412259e2ec969f077706856dfa4409
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-09-25 19:23:41 +0900

    [level 3] Title: 여행경로, Time: 21.56 ms, Memory: 73 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,62 +1,52 @@
 import java.util.*;
-
 class Solution {
-    ArrayList<String[]> rout = new ArrayList<>();
-    String[] oneRout;
-    int index = 0;
-
+    Stack<String> stack = new Stack<>();
     public String[] solution(String[][] tickets) {
-        oneRout = new String[tickets.length + 1];
-        boolean[] useTickets = new boolean[tickets.length];
-        oneRout[0] = "ICN";
-        dfs(tickets, "ICN", useTickets);
-
-        return select();
-    }
-
-    private void dfs(String[][] tickets, String start, boolean[] useTickets) {
-        if (finish(useTickets)) {
-            rout.add(oneRout.clone()); // 기존 배열을 복사해서 추가
-            return;
+        boolean[] visited = new boolean[tickets.length];
+        
+        Arrays.sort(tickets, (o1, o2) -> o1[1].compareTo(o2[1]));
+        
+        stack.push("ICN");
+        
+        for(int i=0; i<tickets.length; i++){
+            String[] str = tickets[i];
+            if(str[0].equals("ICN")){
+                stack.push(str[1]);
+                visited[i]=true;
+                dfs(str[1], tickets, visited);
+            }
+            
+            if(stack.size()==tickets.length+1) break;
+            
+            if(stack.size() > 1) {  // "ICN"은 항상 남겨둠
+                stack.pop();
+            }
+            visited[i]=false;
+        }
+        
+        String[] answer = new String[tickets.length+1];
+        
+        for (int i = stack.size() - 1; i >= 0; i--) {
+            answer[i] = stack.pop();
         }
 
-        for (int i = 0; i < tickets.length; i++) {
-            if (canGo(start, tickets[i]) && !useTickets[i]) {
-                useTickets[i] = true;
-                oneRout[++index] = tickets[i][1];
-                dfs(tickets, tickets[i][1], useTickets);
-                useTickets[i] = false; // 백트래킹
-                index--; // 백트래킹
+        
+        return answer;
+    }
+    
+    private void dfs(String curr, String[][]tickets, boolean[] visited){
+        if(stack.size()==tickets.length+1) return;
+        
+        for(int i=0; i<tickets.length; i++){
+            String[] next = tickets[i];
+            if(!visited[i] && next[0].equals(curr)){
+                stack.push(next[1]);
+                visited[i]=true;
+                dfs(next[1], tickets, visited);
+                visited[i]=false;
+                if(stack.size()!=tickets.length+1) stack.pop();
             }
         }
     }
-
-    private boolean canGo(String start, String[] list) {
-        return start.equals(list[0]);
-    }
-
-    private boolean finish(boolean[] useTickets) {
-        for (boolean b : useTickets) {
-            if (!b) return false;
-        }
-        return true;
-    }
-
-    private String[] select() {
-        String[] first = rout.get(0);
-
-        for (String[] list : rout) {
-            for (int i = 0; i < list.length; i++) {
-                String s1 = first[i];
-                String s2 = list[i];
-                if (s1.compareTo(s2) > 0) {
-                    first = list; // 비교 후 더 작은 알파벳순으로 선택
-                    break;
-                } else if (s1.compareTo(s2) < 0) {
-                    break;
-                }
-            }
-        }
-        return first;
-    }
+    
 }
