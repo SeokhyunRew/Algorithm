5893e6a4ebde20d495197fbe889f30b5ec81f8f6
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-09-26 21:52:44 +0900

    [level 3] Title: 단어 변환, Time: 0.41 ms, Memory: 86.6 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,52 +1,57 @@
-import java.util.Queue;
-import java.util.LinkedList;
+import java.util.*;
+
 class Solution {
     public int solution(String begin, String target, String[] words) {
-        boolean[] visited = new boolean[words.length];
-        Queue<Word> queue = new LinkedList<>();
-        
-        queue.offer(new Word(begin, 0));
-        
-        while(!queue.isEmpty()){
-            
-            Word word = queue.poll();
-            String curr = word.str;
-            int depth = word.depth;
-            
-            if(curr.equals(target)) return depth;
-            
-            for(int i=0; i<words.length; i++){
-                if(visited[i] || !canChange(words[i], curr)) continue;
-                queue.offer(new Word(words[i], depth+1));
-                visited[i]=true;
+        // target이 words에 없으면 바로 실패
+        boolean exists = false;
+        for (String w : words) {
+            if (w.equals(target)) {
+                exists = true;
+                break;
             }
-            
         }
-        
+        if (!exists) return 0;
+
+        boolean[] visited = new boolean[words.length];
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(begin, 0));
+
+        while (!queue.isEmpty()) {
+            Node current = queue.poll();
+
+            if (current.word.equals(target)) {
+                return current.depth;
+            }
+
+            for (int i = 0; i < words.length; i++) {
+                if (!visited[i] && canTransform(current.word, words[i])) {
+                    visited[i] = true;
+                    queue.add(new Node(words[i], current.depth + 1));
+                }
+            }
+        }
         return 0;
     }
-    
-    private boolean canChange(String s1, String s2){
-        char[] arrC1 = s1.toCharArray();
-        char[] arrC2 = s2.toCharArray();
-        int temp = 0;
-        
-        for(int i=0; i<arrC1.length; i++){
-            if(arrC1[i]!=arrC2[i]) temp++;
+
+    // 단어 두 개가 한 글자만 다른지 확인
+    private boolean canTransform(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i)) {
+                diff++;
+                if (diff > 1) return false;
+            }
         }
-        
-        return (temp==1)?true:false;
-        
-        }
-    
-    private class Word{
-        String str;
-        int depth;
-        
-        public Word(String str, int depth){
-            this.str = str;
-            this.depth = depth;
-        }
-        
+        return diff == 1;
+    }
+}
+
+class Node {
+    String word;
+    int depth;
+
+    Node(String word, int depth) {
+        this.word = word;
+        this.depth = depth;
     }
 }
