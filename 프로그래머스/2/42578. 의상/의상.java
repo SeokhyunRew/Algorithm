78f42f23eea55c8f3ad33adedf0e5203ef62e623
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-10-30 18:27:02 +0900

    [level 2] Title: 의상, Time: 0.16 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -4,9 +4,9 @@ class Solution {
         int answer = 1;
         Map<String, Integer> map = new HashMap<>();
         
-        for(int i=0; i<clothes.length; i++) map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
+        for(String[] str : clothes) map.put(str[1], map.getOrDefault(str[1], 0)+1);
         
-        for(Map.Entry <String, Integer> entry : map.entrySet()) answer *= entry.getValue()+1;
+        for(Map.Entry<String, Integer> entry : map.entrySet()) answer *= entry.getValue()+1;
         
         return answer-1;
     }
