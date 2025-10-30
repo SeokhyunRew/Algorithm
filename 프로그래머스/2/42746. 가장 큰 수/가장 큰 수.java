ec91f679722a61f80dce359e613074d739df1aba
SeokhyunRew <141889885+SeokhyunRew@users.noreply.github.com>
2025-10-30 18:22:15 +0900

    [level 2] Title: 가장 큰 수, Time: 259.88 ms, Memory: 161 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,26 +1,23 @@
 import java.util.*;
-
 class Solution {
     public String solution(int[] numbers) {
-        // 1) int[] → String[]
-        String[] arr = new String[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            arr[i] = String.valueOf(numbers[i]);
-        }
+        StringBuilder temp = new StringBuilder();
+        List<Integer> arr = new ArrayList<>();
+        
+        for(int num : numbers) arr.add(num);
+        
+         Collections.sort(arr, (a, b) -> {
+            String sa = String.valueOf(a);
+            String sb = String.valueOf(b);
 
-        // 2) (s1+s2) 와 (s2+s1) 비교 → 내림차순 정렬
-        Arrays.sort(arr, (s1, s2) -> (s2 + s1).compareTo(s1 + s2));
+            return (sb+sa).compareTo(sa+sb);
 
-        // 3) 맨 앞이 "0" 이면 결과 전체가 0 이므로 예외 처리
-        if (arr[0].equals("0")) {
-            return "0";
-        }
-
-        // 4) 순서대로 붙여서 리턴
-        StringBuilder sb = new StringBuilder();
-        for (String s : arr) {
-            sb.append(s);
-        }
-        return sb.toString();
+        });
+        
+        for(int num : arr) temp.append(num);
+        
+        if(temp.charAt(0) == '0') return "0";
+        
+        return temp.toString();
     }
 }
